a188340f277262b7715e54f73e5d3c4ebb26f2a2
Nilesh Bhosale <nilesh@intrior.com>
2017-05-01 09:55:13 +0530

    First Commit, Base Project Setup

---
+++ src/main/java/uba/Application.java
@@ -0,0 +1,16 @@
+package uba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ *
+ * @author Nilesh Bhosale
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
